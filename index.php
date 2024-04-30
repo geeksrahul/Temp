b482bf7420192f2d0a18460aef38ab20acfcde64
geeksrahul <geeksrahulofficial@gmail.com>
2024-04-30 08:06:35 +0530

    edit index.html

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
     <h1>Rahul Baraiya</h1>
     <p>Software Developer</p>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, porro. Harum corporis amet assumenda praesentium quod quae! Cupiditate numquam magni consequatur, iste placeat vitae laborum iusto neque perspiciatis eveniet. Iure ea similique atque aliquid nisi tempore voluptas optio error inventore dolorem. Delectus, omnis neque consequatur in modi quas nesciunt temporibus.</p>
+    <a href="#"></a>
 </body>
 </html>
